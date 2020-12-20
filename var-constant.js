243aa846ac659523a1da9dd326e125501e05dc09
alzarkawy1 <75317483+alzarkawy1@users.noreply.github.com>
2020-12-20 09:40:46 +0700

    add var-constant file

---
+++ var-constant.js
@@ -0,0 +1,14 @@
+const times99 = 99;
+showMessage(times99);
+
+let times99;
+showMessage(times99);
+console.log(times99); 
+let price; 
+price = 49.99;
+price = 29.99;
+showMessage(price);
+   
+  
+const price = 40;
+showMessage(price);
