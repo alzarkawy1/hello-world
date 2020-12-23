6131f7ef8ae9a575c1d9f80aa95f15ffd25c36a1
alzarkawy1 <75317483+alzarkawy1@users.noreply.github.com>
2020-12-23 23:27:07 +0700

    programFlow

---
+++ programFlow.js
@@ -0,0 +1,20 @@
+*    if ( 1 === 1) {
+    showMessage('true');*/
+ let price = 15;
+if (price > 10) {
+   showMessage('true');
+
+};
+else if (price < 5) {
+   showMessage('less then 5');
+}
+ }
+ let price = 20;
+ if (price !== 15) {
+    showMessage('discounted');
+
+let price = 20;
+let message = (price < 10) ? 'yes' : 'No';
+showMessage(message);
+ for (let i=0; i<5; i++){
+   console.log(i); }
