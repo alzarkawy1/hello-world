089d3eb04c944c6d5aaabd0b0473f1b940f29eea
alzarkawy1 <75317483+alzarkawy1@users.noreply.github.com>
2020-12-27 21:56:34 +0700

    add folder

---
+++ js/programFlow.js
@@ -0,0 +1,19 @@
+let price = 12;
+let taxRate = 0.05;
+
+price = price * 3;
+showMessage(++price * taxRate);
+
+let message = "Hello \"World\"";
+showMessage(message);
+
+let name = 'A Thuc';
+let message = 'Chao ${name}';
+showMessage(message);
+
+let message = 'Hello';
+message = message.lowerCase();
+
+let amount = Number.parseFloat("123.123")
+showMessage(amount);
+console.log(amount);
